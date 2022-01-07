9348935e3035770561d0a7b915eaa2ee5b351f9e
Ronan Laker <ronan.laker@gmail.com>
2022-01-07 12:02:16 +0000

    removed env variable settings

--- settings.py
+++ settings.py
@@ -4,7 +4,4 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 DRIVER_PATH = os.getenv('DRIVER_PATH')
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-ERNIE_URL = os.getenv('ERNIE_URL')
 
