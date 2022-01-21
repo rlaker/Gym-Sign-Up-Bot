9c9ffb99b93e0d5297b0ff83eb7095950f8ba4d2
Ronan Laker <ronan.laker@gmail.com>
2022-01-21 15:55:33 +0000

    added default logic

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import time as sleepytime
-import datetime
+from datetime import datetime, timedelta
+import argparse
+import io
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-import argparse
 from settings import DRIVER_PATH, URL
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import io
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common import exceptions as exc
+from selenium.webdriver.firefox.service import Service  
 
 def get_driver_path():
     if(DRIVER_PATH):
@@ -133,62 +133,134 @@ def book_slot(court, day, time, browser, hour = False):
 def confirm(browser):
     WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, 'confirm'))).click()  
     
+def get_default_target_date():
+    weekday_dic = {1: 'Monday',
+               2: 'Tuesday',
+               3: 'Wednesday',
+               4: "Thursday",
+               5: "Friday",
+               6: "Saturday",
+               7: "Sunday"}
+
+    # the script will start running on saturday night, so the weekday should be 6
+    # but our target date is in 29 days time - and should be a sunday
+    script_start_date = datetime.now()
+    if script_start_date.isocalendar().weekday == 5:
+        #29 because the script started on saturday, but want to book when turns midnight and becomes sunday
+        target_date = script_start_date + timedelta(days = 29)
+        
+        #check this is a sunday
+        print(f"Target date is {target_date}, on a {weekday_dic[target_date.isocalendar().weekday]}")
+        
+    elif script_start_date.isocalendar().weekday == 7:
+        print('Script started on the Sunday, maybe you loaded a bit late')
+        print("Will try and book in 28 days still")
+        target_date = script_start_date + timedelta(days = 28)
+        #check this is a sunday
+        if target_date.isocalendar().weekday == 7:
+            print(f"Target date is {target_date} on a {weekday_dic[target_date.isocalendar().weekday]}")
+        else:
+            raise Exception("Started script on Sunday and target date did not come out as a Sunday")
+    else:
+        raise Exception(f'You have started this script on {weekday_dic[script_start_date.isocalendar().weekday]}. \n You have to specify the date instead.')
+        
+    #need to return as a string
+    return target_date.strftime("%Y-%m-%d")
     
-def main(u, pw, day, court, wait_midnight = False):
-    t = datetime.datetime.now()
-    print(f'[STARTING] Signing up for court {court} on {day}')
-    # s=Service(get_driver_path())
+def main(u, pw, day, court, time, wait_midnight = False):
+    """Main script to book the courts with selenium and Firefox/
+
+    Parameters
+    ----------
+    u : str
+        Username
+    pw : str
+        Password
+    day : str
+        Day of court booking in YY-MM-DD format. If None then will use default.
+    court : int
+        Court number
+    time : str
+        Start time of the slot in HH:MM format.
+    wait_midnight : bool, optional
+        If True will wait until midnight before trying to book, by default False
+    """
     
-    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    #browser = webdriver.Firefox(executable_path=get_driver_path()) #webdriver.Chrome(service=s)
+    default = False
+    #if no values set by the user then use the default values
+    if day == None:
+        default = True
+        #get the target booking date
+        day = get_default_target_date()
+        wait_midnight = True
     
+    # keep this condition separate in case you want default day but new court number
+    if court == None:    
+        court = 3
+        
+    if time == None:
+        time = '10:00'
+    
+    print(f'[STARTING] Signing up for court {court} at {time} on {day}')
+    
+    #start the firefox browser
+    # takes a  few seconds
+    browser = webdriver.Firefox(service = Service(DRIVER_PATH))
     browser.get(URL)
     
-    
+    #login to the page before midnight, so we are ready to go
     login(u, pw, browser)
     
-    print(get_day_url)
-    
+    #waits until midnight has passed
     if wait_midnight:
+        now = datetime.now()
+        tomorrow = datetime(now.year, now.month, now.day+1)
+        
         #wait until midnight
-        time  = datetime.datetime.now()
+        time  = datetime.now()
         counter = 0
         print(f"Started waiting at {time}")
-        while time < datetime.datetime(time.year, time.month, time.day+1,0,0):#start of the next day
+        while time < tomorrow:#start of the next day
             if counter == 10:
-                print(time)
+                print(f"Time is {time}, waiting until {tomorrow}")
                 counter = 0
                 
-            time  = datetime.datetime.now()
+            time  = datetime.now()
             sleepytime.sleep(0.5)
             counter += 1
     
     #wait an extra 0.5 seconds before loading the page again
     sleepytime.sleep(0.5)
     
-    #wait for the slot to become bookable
-    
+    #tries to book the first court
     try:
-        book_slot(court, day, '07:00', browser, hour = True)
+            
+        book_slot(court, day, time, browser, hour = True)
+        
         #make sure you comment this line out when testing
-        #confirm(browser)
-    except:
-        #browser.close()
-        raise Exception("failed to book")
-    
-    try:
-        book_slot(court, day, '08:00', browser, hour = False)
-        #make sure you comment this line out when testing
+        #confirm the booking, without pressing it wont book
         #confirm(browser)
     except:
         browser.close()
         raise Exception("failed to book")
     
+    #by default we want to book another half an hour from 11:00 to 11:30
+    if default:
+        try:
+            book_slot(court, day, '11:00', browser, hour = False)
+            
+            #make sure you comment this line out when testing
+            #confirm the booking, without pressing it wont book
+            #confirm(browser)
+        except:
+            browser.close()
+            raise Exception("failed to book")
+    
     
 
     print(f"[SUCCESS] Signed up for court {court}")
     
-    sleepytime.sleep(2)
+    sleepytime.sleep(1)
 
     browser.close()
     
@@ -198,12 +270,13 @@ if __name__ == "__main__":
 
     parser.add_argument('u', type=str, help='Username')
     parser.add_argument('pw', type=str, help='Password')
-    parser.add_argument('day',  type=str, help='Select the day you want to book')
-    parser.add_argument('court',  type=int, help='Select the court you want')
+    parser.add_argument('--day', '-d',  type=str, help='Select the day you want to book')
+    parser.add_argument('--court', '-c',  type=int, help='Select the court you want')
+    parser.add_argument('--time', '-t',  type=str, help='Select the time', )
 
     args = parser.parse_args()
 
-    main(args.u, args.pw, args.day, args.court)
+    main(args.u, args.pw, args.day, args.court, args.time)
 
 
 
